ed89e75a88cd06dda0c14188acdd4254067f4398
Ruvindi Chathurya <133088416+ruvindi-matharaarachchi@users.noreply.github.com>
2026-03-09 23:55:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui')) return 'mui';
+            if (id.includes('recharts')) return 'recharts';
+            if (id.includes('framer-motion')) return 'framer-motion';
+            if (id.includes('jspdf')) return 'jspdf';
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) return 'react';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
